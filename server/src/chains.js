1a4964178a3af39cb28b9c981a2ba53754a8e736
Snowsnowystorm <snowsnowysnow3@gmail.com>
2026-03-20 23:06:36 +1000

    Create chains.js

---
+++ server/src/chains.js
@@ -0,0 +1,14 @@
+// =============================
+// 🌐 SUPPORTED CHAINS
+// =============================
+
+export const CHAINS = {
+  ETH: {
+    name: "Ethereum",
+    rpc: "https://rpc.ankr.com/eth"
+  },
+  BNB: {
+    name: "BNB Smart Chain",
+    rpc: "https://rpc.ankr.com/bsc"
+  }
+};
