751b74bd069b77a47e4e4e23d6509f179a4fc0f1
Snowsnowystorm <snowsnowysnow3@gmail.com>
2026-03-22 04:14:16 +1000

    Create Sidebar.jsx

---
+++ components/Sidebar.jsx
@@ -0,0 +1,15 @@
+export default function Sidebar() {
+  return (
+    <div className="sidebar">
+      <h2 className="logo">💎 Snow Doll</h2>
+
+      <ul>
+        <li>Dashboard</li>
+        <li>Wallet</li>
+        <li>Bridge</li>
+        <li>Transactions</li>
+        <li>Admin 👑</li>
+      </ul>
+    </div>
+  );
+}
