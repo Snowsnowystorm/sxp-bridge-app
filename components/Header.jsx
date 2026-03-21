7161c30615586827d1e0a584f4e8049d808a973c
Snowsnowystorm <snowsnowysnow3@gmail.com>
2026-03-22 05:09:31 +1000

    Delete components/Header.jsx

--- components/Header.jsx
+++
@@ -1,8 +0,0 @@
-export default function Header() {
-  return (
-    <div className="header">
-      <div>Welcome back 💎</div>
-      <button className="glow-btn">Connect Wallet</button>
-    </div>
-  );
-}
