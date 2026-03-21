1896e272e1436ba869cca4d5cf042d4c72654182
Snowsnowystorm <snowsnowysnow3@gmail.com>
2026-03-22 06:19:05 +1000

    Create Header.jsx

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,8 @@
+export default function Header() {
+  return (
+    <div className="card glow" style={{ marginBottom: 20 }}>
+      <h1>💎 SXP Exchange</h1>
+      <p>Snow Doll Luxury Trading Interface 🦋</p>
+    </div>
+  );
+}
