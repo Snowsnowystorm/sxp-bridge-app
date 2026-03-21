e6be104e43c253d3497d0bb9ee015ad0cd838c4f
Snowsnowystorm <snowsnowysnow3@gmail.com>
2026-03-22 04:16:51 +1000

    Create ChartPanel.jsx

---
+++ components/ChartPanel.jsx
@@ -0,0 +1,10 @@
+export default function ChartPanel() {
+  return (
+    <div className="card big">
+      <h3>📈 Market Activity</h3>
+      <div className="chart-placeholder">
+        (TradingView / Chart.js goes here)
+      </div>
+    </div>
+  );
+}
