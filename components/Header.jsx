ea1f3d2ff4bb84792f106fe23eb99db3e0bce1e9
Snowsnowystorm <snowsnowysnow3@gmail.com>
2026-03-22 04:14:45 +1000

    Create Header.jsx

---
+++ components/Header.jsx
@@ -0,0 +1,8 @@
+export default function Header() {
+  return (
+    <div className="header">
+      <div>Welcome back 💎</div>
+      <button className="glow-btn">Connect Wallet</button>
+    </div>
+  );
+}
