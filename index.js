ea958b180dad6c676ffe69cd58bb5f71979c5151
Snowsnowystorm <snowsnowysnow3@gmail.com>
2026-03-22 01:09:47 +1000

    Create index.js

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({ status: "SXP BACKEND LIVE 💎" });
+});
+
+app.get("/test", (req, res) => {
+  res.json({ message: "API working 🚀" });
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log("Server running on port " + PORT);
+});
