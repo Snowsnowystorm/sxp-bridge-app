6afc29a675019e2d0f7c308648f947d2c7037036
Snowsnowystorm <snowsnowysnow3@gmail.com>
2026-03-22 05:12:42 +1000

    Create Sidebar.jsx

---
+++ client/src/components/Sidebar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Sidebar() {
+  return (
+    <div className="sidebar">
+      <p>💎 Dashboard</p>
+      <p>🌉 Bridge</p>
+      <p>💸 Withdraw</p>
+      <p>📊 History</p>
+    </div>
+  );
+}
