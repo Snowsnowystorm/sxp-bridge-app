aa1c06c6d1e52c795152bc19bb3de47e27c03859
Snowsnowystorm <snowsnowysnow3@gmail.com>
2026-03-25 17:51:41 +1000

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+console.log("🔥 SERVER FILE LOADED");
+try {
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
@@ -224,3 +226,6 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
 });
+   } catch (err) {
+  console.error("🔥 FATAL ERROR:", err);
+}
